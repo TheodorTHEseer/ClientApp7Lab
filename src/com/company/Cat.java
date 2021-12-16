aa52d2f85ac3eb178a6867a3fa5c376e963747fc
fedor <fedor.prozorovV@gmail.com>
2021-12-16 13:37:20 +0300

    alpha

--- src/com/company/Cat.java
+++ src/com/company/Cat.java
@@ -1,35 +1,10 @@
 package com.company;
 
-import com.company.logs;
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 import static com.company.logs.home;
 
 public class Cat implements Serializable {
-    private static final Map <String, String> nameRace = new HashMap<>();
-    private static final Map <String, Double> nameParams = new HashMap<>();
-    private static final String [] names = {"Лютик", "Барсик", "Мурка", "Тигра", "Летиция", "Аэлита"};
-    private static final double [] params = {1.2, 2.3, 4.3, 1.7, 2.3, 3.3};
-    private static void load(){
-        nameRace.put(names[0], "Abyssinian");
-        nameRace.put(names[1], "Persian");
-        nameRace.put(names[2], "Brazilian Shorthair");
-        nameRace.put(names[3], "Burmilla");
-        nameRace.put(names[4], "York");
-        nameRace.put(names[5], "British Longhair");
-        for (int count = 0; count<names.length; count++){
-            nameParams.put(names[count], params[count]);
-        }
-
-    }
-    public static void loadCats(){
-        load();
-    }
     private String name;
     private String race;
     private double weight;
@@ -67,27 +42,6 @@ public class Cat implements Serializable {
         catch (IOException ioException) {
             ioException.printStackTrace();
             logs.add("Cat|upload|Failed");
-        }
-    }
-    public void download(){
-        String catString=null;
-        try {
-            FileReader fileReader = new FileReader(home + File.separator + "Desktop" + File.separator +
-                    "testGameFolder" + File.separator + "SettlementBuildings.txt");
-            Scanner scanner = new Scanner(fileReader);
-            catString=scanner.nextLine();
-            fileReader.close();
-            logs.add("Cat|download|1|Done");
-        }
-        catch (Exception exception){
-            logs.add("Cat|download|1|Failed");
-        }
-        try {
-            fromString(catString);
-            logs.add("Cat|download|2|Done");
-        }
-        catch (Exception e) {
-            logs.add("Cat|download|2|Failed");
         }
     }
 }
